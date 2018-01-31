5186cf677cf0ec671f0293b8002af4c1ca7e1bcb
David Howden <dhowden@gmail.com>
2018-01-31 12:44:13 +1100

    initial commit

---
+++ sajari-sdk-csharp/Response.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sajari
+{
+    public class Response
+    {
+        private Token[] tokens;
+
+        public Response(Sajari.Api.Query.V1.SearchResponse response)
+        {
+            foreach (var t in response.Tokens) {
+                t.TokenCase.GetType();
+            }
+        }
+    }
+}
